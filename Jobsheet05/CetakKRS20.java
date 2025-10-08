6e5f42e29b75cfd772b630c6757a84e4f82e56bd
Najwa Viola Hanafi <kimviolatte@gmail.com>
2025-10-08 10:02:43 +0700

    Modifikasi Percobaan 1

---
+++ Jobsheet05/CetakKRS20.java
@@ -0,0 +1,21 @@
+package Jobsheet05;
+
+import java.util.Scanner;
+public class CetakKRS20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //variabel UKT Lunas
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.println("Apakah UKT sudah lunas? (true/false):");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT sudah terverfikasi. ");
+            System.out.println("Silahkan cetak KRS Anda dan minta tanda tangan DPA ");
+        }
+        else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+        }
+        }
+        }
