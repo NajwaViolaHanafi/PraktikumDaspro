c7278f8fea99a7a9791a5e083373b6769ab11aee
Najwa Viola Hanafi <kimviolatte@gmail.com>
2025-11-07 20:32:57 +0700

    modifikasi  kelompok dengan rata-rata nilai tertinggi

--- Jobsheet08/NilaiKelompok20.java
+++ Jobsheet08/NilaiKelompok20.java
@@ -3,34 +3,30 @@ import java.util.Scanner;
 public class NilaiKelompok20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i, j;
+        int i, j; 
         double totalNilai, rataNilai, nilai;
-
-    for (j=1;j<=5;j++) {
-        totalNilai = 0;
-
-    for (j = 1; j<= 5; j++) {
-        System.out.println("Masukkan nilai ke-" + j + ": ");
-        nilai = sc.nextDouble();
-        totalNilai += nilai;
-        rataNilai=totalNilai/5;
+        double rataNilaiTertinggi = 0;       
+        int kelompokTertinggi = 0; 
 
         i = 1;
-
-    while (i <= 6) {
-        System.out.println("Kelompok " + i);
-        totalNilai = 0;
-
+        while (i <= 6) { 
+            totalNilai = 0; 
+            System.out.println("\nKelompok " + i);
         for (j = 1; j <= 5; j++) {
-            System.out.print("Masukkan nilai ke-" + j + ": ");
-            nilai = sc.nextDouble();
-            totalNilai += nilai;
+                System.out.print("Masukkan nilai dari penilai ke-" + j + ": ");
+                nilai = sc.nextDouble();
+                totalNilai += nilai;
+            }
+            rataNilai = totalNilai / 5;
+            System.out.println("Rata-rata nilai kelompok " + i + " = " + rataNilai);
+
+            if (rataNilai > rataNilaiTertinggi) {
+                rataNilaiTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+            i++;
         }
-        rataNilai = totalNilai/5;
-        System.out.println("Rata-rata: " + rataNilai);
-        i++;
-    }
-    }
-    }
+            System.out.println("Kelompok dengan rata-rata tertinggi adalah Kelompok "
+                + kelompokTertinggi + " dengan nilai rata-rata " + rataNilaiTertinggi);
     }
 }
