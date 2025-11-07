4602e848288ef6b0a3d03f6a1cff1614dc051d1e
Najwa Viola Hanafi <kimviolatte@gmail.com>
2025-11-07 20:37:42 +0700

    Modifikasi perulangan WHILE menjadi perulangan FOR

--- Jobsheet08/NilaiKelompok20.java
+++ Jobsheet08/NilaiKelompok20.java
@@ -3,30 +3,28 @@ import java.util.Scanner;
 public class NilaiKelompok20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i, j; 
         double totalNilai, rataNilai, nilai;
-        double rataNilaiTertinggi = 0;       
-        int kelompokTertinggi = 0; 
+        double rataNilaiTertinggi = 0; 
+        int kelompokTertinggi = 0;     
 
-        i = 1;
-        while (i <= 6) { 
-            totalNilai = 0; 
-            System.out.println("\nKelompok " + i);
-        for (j = 1; j <= 5; j++) {
-                System.out.print("Masukkan nilai dari penilai ke-" + j + ": ");
-                nilai = sc.nextDouble();
-                totalNilai += nilai;
-            }
-            rataNilai = totalNilai / 5;
-            System.out.println("Rata-rata nilai kelompok " + i + " = " + rataNilai);
+    for (int i = 1; i <= 6; i++) {
+        totalNilai = 0; 
+        System.out.println("Kelompok " + i);
 
-            if (rataNilai > rataNilaiTertinggi) {
-                rataNilaiTertinggi = rataNilai;
-                kelompokTertinggi = i;
-            }
-            i++;
+    for (int j = 1; j <= 5; j++) {
+        System.out.print("Masukkan nilai dari penilai ke-" + j + ": ");
+        nilai = sc.nextDouble();
+        totalNilai += nilai;
+    }
+        rataNilai = totalNilai / 5;
+        System.out.println("Rata-rata nilai kelompok " + i + " = " + rataNilai);
+        
+    if (rataNilai > rataNilaiTertinggi) {
+        rataNilaiTertinggi = rataNilai;
+        kelompokTertinggi = i;
         }
-            System.out.println("Kelompok dengan rata-rata tertinggi adalah Kelompok "
+    }
+        System.out.println("\nKelompok dengan rata-rata tertinggi adalah Kelompok "
                 + kelompokTertinggi + " dengan nilai rata-rata " + rataNilaiTertinggi);
     }
 }
